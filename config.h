daaad1311a29a611ad941c0c2adb03138036203c
arnavgr <arnavgr@hotmail.com>
2024-11-25 13:15:28 +0530

    made changes to work on voidpup64

--- config.h
+++ config.h
@@ -9,68 +9,41 @@ static const char unknown_str[] = "n/a";
 /* maximum output string length */
 #define MAXLEN 2048
 
-/*
- * function            description                     argument (example)
- *
- * battery_perc        battery percentage              battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * battery_remaining   battery remaining HH:MM         battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * battery_state       battery charging state          battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * cat                 read arbitrary file             path
- * cpu_freq            cpu frequency in MHz            NULL
- * cpu_perc            cpu usage in percent            NULL
- * datetime            date and time                   format string (%F %T)
- * disk_free           free disk space in GB           mountpoint path (/)
- * disk_perc           disk usage in percent           mountpoint path (/)
- * disk_total          total disk space in GB          mountpoint path (/)
- * disk_used           used disk space in GB           mountpoint path (/)
- * entropy             available entropy               NULL
- * gid                 GID of current user             NULL
- * hostname            hostname                        NULL
- * ipv4                IPv4 address                    interface name (eth0)
- * ipv6                IPv6 address                    interface name (eth0)
- * kernel_release      `uname -r`                      NULL
- * keyboard_indicators caps/num lock indicators        format string (c?n?)
- *                                                     see keyboard_indicators.c
- * keymap              layout (variant) of current     NULL
- *                     keymap
- * load_avg            load average                    NULL
- * netspeed_rx         receive network speed           interface name (wlan0)
- * netspeed_tx         transfer network speed          interface name (wlan0)
- * num_files           number of files in a directory  path
- *                                                     (/home/foo/Inbox/cur)
- * ram_free            free memory in GB               NULL
- * ram_perc            memory usage in percent         NULL
- * ram_total           total memory size in GB         NULL
- * ram_used            used memory in GB               NULL
- * run_command         custom shell command            command (echo foo)
- * swap_free           free swap in GB                 NULL
- * swap_perc           swap usage in percent           NULL
- * swap_total          total swap size in GB           NULL
- * swap_used           used swap in GB                 NULL
- * temp                temperature in degree celsius   sensor file
- *                                                     (/sys/class/thermal/...)
- *                                                     NULL on OpenBSD
- *                                                     thermal zone on FreeBSD
- *                                                     (tz0, tz1, etc.)
- * uid                 UID of current user             NULL
- * uptime              system uptime                   NULL
- * username            username of current user        NULL
- * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
- *                                                     NULL on OpenBSD/FreeBSD
- * wifi_essid          WiFi ESSID                      interface name (wlan0)
- * wifi_perc           WiFi signal in percent          interface name (wlan0)
- */
+/* Custom commands */
+static const char ethernet_status[] = 
+    "ethernet_iface=$(ip link show | grep -E '^[0-9]+: (en|eth)' | awk '{print $2}' | sed 's/://'); \
+    if [ -n \"$ethernet_iface\" ]; then \
+        if [ \"$(cat /sys/class/net/$ethernet_iface/operstate)\" = \"up\" ]; then \
+            echo \"$ethernet_iface\"; \
+        fi; \
+    fi";
+
+static const char wifi_status[] = 
+    "wifi_iface=$(iw dev | grep -oP 'Interface \K\w+'); \
+    if [ -n \"$wifi_iface\" ]; then \
+        if [ \"$(cat /sys/class/net/$wifi_iface/operstate)\" = \"up\" ]; then \
+            wifi_ssid=$(iw dev $wifi_iface link | grep 'SSID' | awk '{print $2}'); \
+            if [ -n \"$wifi_ssid\" ]; then \
+                echo \"$wifi_ssid\"; \
+            fi; \
+        fi; \
+    fi";
+
+static const char swap_status[] = 
+    "swap_usage=$(free -h | grep Swap | awk '{print $3 \"/\" $2}'); echo $swap_usage";
+
+static const char ram_status[] = 
+    "ram_used=$(free -h | grep Mem | awk '{print $3}'); \
+    ram_total=$(free -h | grep Mem | awk '{print $2}'); \
+    echo \"$ram_used/$ram_total\"";
+
+/* function format argument */
 static const struct arg args[] = {
-	/* function format          argument */
-	      { run_command, "  %s", "checkupdates | grep -c -"},
-        { wifi_essid, " 󰖩 %s", "wlp2s0"},
-        { cpu_perc, " 󰒋 %s", NULL },
-        { ram_used, "  %s", NULL},
-	      { run_command, "  %s", "pamixer --get-volume-human"},
-	      { run_command, " %s", "batinfo"},
-  /*      { battery_perc, "   %s", "BAT0" },  */
-	      { datetime, "  %s", "%a %d %B  %I:%M %p"},
+    { cpu_perc,           " %s%%",       NULL },  // CPU usage with percentage symbol
+    { run_command,        " 󰈀 %s",         ethernet_status },  // Ethernet status if connected
+    { run_command,        "  %s",         wifi_status },      // WiFi SSID if connected
+    { run_command,        " 󰯍 %s",            swap_status },     // Swap used/total
+    { run_command,        "  %s",            ram_status },      // RAM used/total
+    { datetime,           "  %s",         "%a %d %B  %I:%M %p" }, // Date and time
 };
+
